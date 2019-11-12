ac726430dc0c5a54194b4042722c3ae87b0dbed2
s-nakai <s-nakai@nisso.co.jp>
2019-11-12 15:42:15 +0900

    fix accordions

--- js_sources/main.js
+++ js_sources/main.js
@@ -3,12 +3,8 @@ const $ = require('jquery');
 $(() => {
     $('.faq-question').each(function() {
         $(this).on('click', function() {
-            $("+.faq-answer", this).slideToggle("normal", function() {
-                if ($(this).is(':visible')) {
-                    $(this).css("display", "flex");
-                }
-            });
-        $(this).find(".faq-arrow").toggleClass("active");
+            $("+.faq-answer", this).slideToggle("normal");
+            $(this).find(".faq-arrow").toggleClass("active");
         });
     });
 });
