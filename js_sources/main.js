85efbde8d4a324042cf97c1fa030c968379a985f
s-nakai <s-nakai@nisso.co.jp>
2019-11-22 15:31:42 +0900

    add check userAjent

--- js_sources/main.js
+++ js_sources/main.js
@@ -1,5 +1,15 @@
 import $ from 'jquery';
 
+const UA = navigator.userAgent.toLowerCase();
+const uaCheck = agent => UA.indexOf(agent) == -1;
+const mobileDevise = ['iphone', 'ipad', 'ipod', 'iemobile', 'tizen', 'android'];
+const checkedDevise = mobileDevise.filter(devise => {
+    return uaCheck(devise);
+});
+if (checkedDevise.length > 0) {
+    location.href = 'https://www.717450.net/priority/nissanshatai_kyushu/'
+};
+
 $(() => {
     $('.faq-question').each(function() {
         $(this).on('click', function() {
@@ -8,3 +18,4 @@ $(() => {
         });
     });
 });
+
